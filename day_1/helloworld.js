8eebeaed87c77d32292f0eea5435547142e491a6
A-Hicken <amberlie.hicken@gmail.com>
2023-10-18 14:29:54 -0600

    first commit

---
+++ day_1/helloworld.js
@@ -0,0 +1,16 @@
+console.log("Hello, World!");
+/*console.log(typeof "Asabeneh"); // string
+console.log(typeof 5); // number
+console.log(typeof true); // boolean
+console.log(typeof null); // object type
+console.log(typeof undefined); // undefined
+//commenting the code itself with a single comment
+//let firstName= 'Asabeneh'; single line comment
+// let last Name = 'Yetayeh'; single line comment
+
+/*
+let location = 'Helsinki';
+let age = 100;
+let isMarried = true;
+This is a Multiple line comment
+*/
